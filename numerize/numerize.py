423e122c5f08e9507cb891f13523f6164b252e86
unknown <erik.kempmann@gmx.de>
2021-07-22 15:30:46 +0200

    Making numerized code smaller

--- numerize/numerize.py
+++ numerize/numerize.py
@@ -35,35 +35,16 @@ def numerize(n, decimals=2):
     1,000,000,000 -> 1B
     1,000,000,000,000 -> 1T
     '''
+    out_if_fail = n
     is_negative_string = ""
     if n < 0:
         is_negative_string = "-"
     n = abs(Decimal(n))
-    if n < 1000:
-        return is_negative_string + str(drop_zero(round_num(n, decimals)))
-    elif n >= 1000 and n < 1000000:
-        if n % 1000 == 0:
-            return is_negative_string + str(int(n / 1000)) + "K"
+    str_list = ["","K", "M", "B", "T"]
+    for i in str_list:
+        if n < 1000:
+            return is_negative_string + str(drop_zero(round_num(n, decimals))) + i
         else:
-            n = n / 1000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "K"
-    elif n >= 1000000 and n < 1000000000:
-        if n % 1000000 == 0:
-            return is_negative_string + str(int(n / 1000000)) + "M"
-        else:
-            n = n / 1000000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "M"
-    elif n >= 1000000000 and n < 1000000000000:
-        if n % 1000000000 == 0:
-            return is_negative_string + str(int(n / 1000000000)) + "B"
-        else:
-            n = n / 1000000000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "B"
-    elif n >= 1000000000000 and n < 1000000000000000:
-        if n % 1000000000000 == 0:
-            return is_negative_string + str(int(n / 1000000000000)) + "T"
-        else:
-            n = n / 1000000000000
-            return is_negative_string + str(drop_zero(round_num(n, decimals))) + "T"
-    else:
-        return is_negative_string + str(n)
+            n = n/1000
+        
+    return(str(out_if_fail))
